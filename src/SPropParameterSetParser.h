f0aac2b75ad286eb2b648f1104fb62c43d775e3a
RayanWang <shalo813.sw@gmail.com>
2016-03-09 22:50:06 +0800

    Update.

--- src/SPropParameterSetParser.h
+++ src/SPropParameterSetParser.h
@@ -1,17 +1,3 @@
-/*
- * Delta CONFIDENTIAL
- *
- * (C) Copyright Delta Electronics, Inc. 2014 All Rights Reserved
- *
- * NOTICE:  All information contained herein is, and remains the
- * property of Delta Electronics. The intellectual and technical
- * concepts contained herein are proprietary to Delta Electronics
- * and are protected by trade secret, patent law or copyright law.
- * Dissemination of this information or reproduction of this material
- * is strictly forbidden unless prior written permission is obtained
- * from Delta Electronics.
- */
-
 #pragma once
 
 #include "BitStreamReader.h"
